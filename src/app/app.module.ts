226d8a2814a5e022d70ed4e0326956e751ac075f
EL MAHDI BENZINA <benzina.elmahdi@gmail.com>
2022-05-05 11:34:02 +0000

    step 1 : Use Leaflet in your Angular 10 project for interactive maps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+
+import {LeafletModule} from '@asymmetrik/ngx-leaflet';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LeafletModule
   ],
   providers: [],
   bootstrap: [AppComponent]
